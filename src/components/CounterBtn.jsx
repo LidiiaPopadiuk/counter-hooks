1b10673681c98bc23b79fb27e942f1211b6ed69f
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2025-12-14 17:57:12 +0100

    Updates

--- src/components/CounterBtn.jsx
+++
@@ -1,5 +0,0 @@
-export const Button = ({titleBtn, changeNumber}) => {
-    return(
-        <button style={{padding: '10px 20px', backgroundColor: 'lime'}} onClick={changeNumber}>{titleBtn}</button>
-    )
-}
